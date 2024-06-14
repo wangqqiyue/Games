52d60e2842292b4eebb428c61d1779be7f67f794
wangqqiyue <1796483016@qq.com>
2024-06-14 15:54:37 +0800

    [feature] 初步完成飞船飞行旋转功能

--- Asteroids/Asteroid.cpp
+++ Asteroids/Asteroid.cpp
@@ -2,3 +2,61 @@
 #define INCLUDE_ASTEROID
 #include "Asteroid.h"
 #endif 
+
+void Asteroid::WrapCoordinates(float ix, float iy, float& ox, float& oy) {
+	ox = ix;
+	oy = iy;
+	if (ix < 0.0f)	ox = ix + (float)ScreenWidth();
+	if (ix >= (float)ScreenWidth())	ox = ix - (float)ScreenWidth();
+	if (iy < 0.0f)	oy = iy + (float)ScreenHeight();
+	if (iy >= (float)ScreenHeight()) oy = iy - (float)ScreenHeight();
+}
+
+bool Asteroid::KeyHit(float fElapsedTime) {
+	if (GetKey(olc::UP).bHeld) {
+		spaceship.dx += sinf(spaceship.angle)* fElapsedTime;
+		spaceship.dy += -cosf(spaceship.angle)* fElapsedTime;
+	}
+	if (GetKey(olc::DOWN).bHeld) {
+		spaceship.dx -= sinf(spaceship.angle) * fElapsedTime;
+		spaceship.dy -= -cosf(spaceship.angle) * fElapsedTime;
+	}
+	if (GetKey(olc::LEFT).bHeld) {
+		spaceship.angle -= 1.0f* fElapsedTime;
+	}
+	if (GetKey(olc::RIGHT).bHeld) {
+		spaceship.angle += 1.0f* fElapsedTime;
+	}
+
+	return true;
+}
+
+void Asteroid::DrawWireFrame(SpaceObject &sObject) {
+	WrapCoordinates(sObject.x, sObject.y, sObject.x, sObject.y);
+	vector<pair<float, float>> psTranslated=sObject.points;
+	int psNumber = sObject.points.size();
+	float angle = sObject.angle;
+	//rotate
+	for (int i = 0; i < psNumber; i++) {
+		psTranslated[i].first = sObject.points[i].first * cosf(angle)-sObject.points[i].second*sinf(angle);
+		psTranslated[i].second = sObject.points[i].first * sinf(angle) + sObject.points[i].second * cosf(angle);
+	}
+
+
+	//scale
+	for (int i = 0; i < psNumber; i++) {
+		psTranslated[i].first *= sObject.nSize;
+		psTranslated[i].second *= sObject.nSize;
+	}
+
+	//translated to Screen World
+	for (int i = 0; i < psNumber; i++) {
+		psTranslated[i].first += sObject.x;
+		psTranslated[i].second += sObject.y;
+	}
+
+	for (int i = 0; i < psNumber; i++) {
+		int next = (i + 1) % psNumber;
+		DrawLine(psTranslated[i].first, psTranslated[i].second, psTranslated[next].first, psTranslated[next].second);
+	}
+}
--- Asteroids/Asteroid.h
+++ Asteroids/Asteroid.h
@@ -1,18 +1,65 @@
 #pragma once
 #include "olcPixelGameEngine.h"
+#include <vector>
+#include <utility>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+using std::vector;
+using std::pair;
+using std::make_pair;
+
+class SpaceObject {
+public:
+	float x, y, dx, dy, angle;
+	int nSize;
+
+	vector<pair<float, float>> points;
+	SpaceObject() = default;
+	SpaceObject(float x, float y, float dx, float dy, float angle, vector<pair<float, float>>ps,int size) {
+		this->x = x;
+		this->y = y;
+		this->dx = dx;
+		this->dy = dy;
+		this->angle = angle;
+		this->points = ps;
+		this->nSize = size;
+	}
+	void move() {
+		x += dx;
+		y += dy;
+	}
+};
 
 class Asteroid:public olc::PixelGameEngine
 {
+	
 public:
+	SpaceObject spaceship;
+	vector<SpaceObject> asteroids;
+	vector<SpaceObject> bulletes;
+	int score;
+
 	Asteroid() {
 		sAppName = "Asteroid";
 	}
 	bool OnUserCreate()override {
+		spaceship = { ScreenWidth()/2.0f,ScreenHeight()/2.0f,0.0f,0.0f,0.0f,{make_pair(0,-5),make_pair(-2.5,2.5),make_pair(2.5,2.5)},5};
 		return true;
 	};
 
 	bool OnUserUpdate(float fElapsedTime)override{
+		Clear(olc::BLACK);
+		KeyHit(fElapsedTime);
+		spaceship.move();
+		
+		DrawWireFrame(spaceship);
 		return true;
 	};
+
+	void WrapCoordinates(float ix, float iy, float& ox, float& oy);
+	bool KeyHit(float fElapsedTime);
+	void DrawWireFrame(SpaceObject &sObject);
 };
 
