70237eb1b2aa84a8908fa18489ad55871b725394
wangqqiyue <1796483016@qq.com>
2024-06-15 10:40:08 +0800

    [fix] 解决游戏启动后焦点不在游戏窗口的bug

--- Asteroids/main.cpp
+++ Asteroids/main.cpp
@@ -5,7 +5,14 @@
 
 int main() {
 	Asteroid asteroid;
+	
+
 	if (asteroid.Construct(300, 200, 2, 2)) {
+		// 获取当前活动窗口的句柄
+		HWND hWnd = GetForegroundWindow();
+
+		// 将当前活动窗口设置为焦点
+		SetForegroundWindow(hWnd);
 		asteroid.Start();
 	}
 }
