ea063513456c270afe42388e223aafe07f910f17
wangqqiyue <1796483016@qq.com>
2024-07-19 10:42:48 +0800

    [add] 多线程服务器

--- SDL_net_learning/server.cpp
+++ SDL_net_learning/server.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <thread>
 #include "SDL.h"
 #include "SDL_net.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::thread;
+
+void doHandler(TCPsocket client_sock) {
+	IPaddress* client_ip = SDLNet_TCP_GetPeerAddress(client_sock);
+	unsigned char* client_ip_str = (unsigned char*)&client_ip->host;
+	int port= client_ip->port;
+	char buf[512] = { 0 };
+	int len = 0;
+	bool quit = false;
+	
+	printf("已与客户端%d.%d.%d.%d:%d 建立连接",
+		client_ip_str[0], client_ip_str[1], client_ip_str[2], client_ip_str[3],port);
+	while (!quit) {
+
+		len = SDLNet_TCP_Recv(client_sock, buf, 512);
+		buf[len] = 0;
+		printf("来自%d.%d.%d.%d:port",
+			client_ip_str[0], client_ip_str[1], client_ip_str[2], client_ip_str[3],port);
+		cout << buf << endl;
+		if (0 == strcmp(buf, "exit")) {
+			printf("%d.%d.%d.%d:%d 已退出\n",
+				client_ip_str[0], client_ip_str[1], client_ip_str[2], client_ip_str[3],port);
+			quit = true;
+		}
+	}
+	SDLNet_TCP_Close(client_sock);
+}
 
 int main(int argc, char* argv[]) {
 	IPaddress ip;
@@ -18,24 +46,8 @@ int main(int argc, char* argv[]) {
 	cout << "等待客户端连接中...." << endl;
 	while (true) {
 		if (client_sock = SDLNet_TCP_Accept(server_sock)) {
-			IPaddress *client_ip = SDLNet_TCP_GetPeerAddress(client_sock);
-			unsigned char* client_ip_str = (unsigned char *)&client_ip->host;
-			printf("开始接受来自客户端%d.%d.%d.%d的消息\n",
-				client_ip_str[0], client_ip_str[1], client_ip_str[2], client_ip_str[3]);
-
-			char buf[512]={0};
-			int len = 0;
-			bool quit = false;
-			while (!quit) {
-				
-				len = SDLNet_TCP_Recv(client_sock, buf, 512);
-				buf[len] = 0;
-				cout << "消息：" << buf << endl;
-				if (0 == strcmp(buf, "exit")) {
-					cout << "客户端已退出." << endl;
-					quit = true;
-				}
-			}
+			thread t(doHandler, client_sock);
+			t.detach();
 		}
 	}
 	SDLNet_TCP_Close(server_sock);
