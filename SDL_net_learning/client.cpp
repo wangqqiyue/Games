195e46b0ea5932d633eb3c2a3c38ba869b7891b5
wangqqiyue <1796483016@qq.com>
2024-07-21 21:31:44 +0800

    [fix] 修复了客户端关闭时,会导致服务器挂死的bug

--- SDL_net_learning/client.cpp
+++ SDL_net_learning/client.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <thread>
 #include <Windows.h>
+#include <signal.h>
 
 using std::cout;
 using std::endl;
@@ -10,38 +11,6 @@ using std::cin;
 using std::thread;
 bool g_quit = false;
 
-int clearLastLine() {
-	HANDLE hConsole;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	DWORD written;
-	COORD lastLinePos;
-
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (hConsole == INVALID_HANDLE_VALUE) {
-		printf("Failed to get console handle.\n");
-		return 1;
-	}
-
-	// 获取控制台屏幕缓冲区信息
-	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
-		printf("Failed to get console screen buffer info.\n");
-		return 1;
-	}
-	
-
-	// 计算最后一行的位置
-	lastLinePos.X = 0;
-	lastLinePos.Y = csbi.dwSize.Y - 1;
-	// 移动光标到最后一行
-	SetConsoleCursorPosition(hConsole, lastLinePos);
-
-	// 清空当前行
-	FillConsoleOutputCharacterA(hConsole, ' ', csbi.dwSize.X, lastLinePos, &written);
-	FillConsoleOutputAttribute(hConsole, csbi.wAttributes, csbi.dwSize.X, lastLinePos, &written);
-
-	// 再次移动光标到最后一行开始
-	SetConsoleCursorPosition(hConsole, lastLinePos);
-}
 
 //接收数据处理线程
 void dohandler(TCPsocket sd)
@@ -78,31 +47,49 @@ void dohandler(TCPsocket sd)
 		}
 	}
 }
+TCPsocket g_socket = NULL;
+
+//系统信号钩子
+BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
+{
+	switch (fdwCtrlType)
+	{
+		// CTRL-CLOSE: 关闭窗口时存储当前信息到外部文件,再退出系统 
+	case CTRL_CLOSE_EVENT:
+		SDLNet_TCP_Send(g_socket, "exit", 4);
+		SDLNet_TCP_Close(g_socket);
+		SDLNet_Quit();
+		return TRUE;
+
+	default:
+		return FALSE;
+	}
+}
 
 int main(int argc, char* argv[]) {
 	IPaddress ip;
-	TCPsocket socket;
+	SetConsoleCtrlHandler(CtrlHandler, TRUE);
 
 	if (0 != SDLNet_Init()) {
 		//初始化失败
 		return 0;
 	}
-	atexit(SDLNet_Quit);//注册一个程序终止时执行的退出函数
+
 
 	const char* dstip = "116.198.37.2";//116.198.37.2  192.168.3.128
 	SDLNet_ResolveHost(&ip, dstip, 8888);
 	
-	socket = SDLNet_TCP_Open(&ip);
+	g_socket = SDLNet_TCP_Open(&ip);
 	char buf[512] = { 0 };
 	
-	if (socket) {
+	if (g_socket) {
 		cout << "已连接到服务器." << endl;
 		cout << "请输入你的消息，以exit退出服务器" << endl;
 	}
 	else {
 		cout << "无法连接到服务器" << endl;
 	}
-	thread th(dohandler, socket);//线程处理
+	thread th(dohandler, g_socket);//线程处理
 	th.detach();
 
 	Sleep(3000);
@@ -112,14 +99,14 @@ int main(int argc, char* argv[]) {
 		memset(buf, 0, 512);
 		gets_s(buf, 512);
 		int len = strlen(buf);
-		SDLNet_TCP_Send(socket, buf, len);
+		SDLNet_TCP_Send(g_socket, buf, len);
 
 		if (0 == strcmp(buf, "exit")) {
 			g_quit = true;
 			break;
 		}
 	}
-	SDLNet_TCP_Close(socket);
+	SDLNet_TCP_Close(g_socket);
 	SDLNet_Quit();
 
 	return 0;
